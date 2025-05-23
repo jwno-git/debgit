494ffc2c9b9fbcd1dda5ebd017f9eef37836c421
jwno-git <161667580+jwno-git@users.noreply.github.com>
2025-05-23 10:21:07 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ read
 echo
 echo "=== Updating Repo's and installing Apps ==="
 
-sudo apt install curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
