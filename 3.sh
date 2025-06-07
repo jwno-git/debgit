1467efdfcaf39f7deb6c449660b89bcabdffc90f
jwno-git <161667580+jwno-git@users.noreply.github.com>
2025-06-06 20:57:35 -0600

    Create 3.sh

---
+++ 3.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+set -e
+
+sleep 1.0
+
+sudo apt install -y \
+  bluez \
+  brave-browser \
+  brightnessctl \
+  btop \
+  chafa \
+  cliphist \
+  dunst \
+  fastfetch \
+  fbset \
+  firefox-esr-l10n-en-ca \
+  flatpak \
+  fonts-font-awesome \
+  fonts-terminus \
+  gimp \
+  grim \
+  hyprland \
+  imagemagick \
+  jq \
+  lf \
+  lxpolkit \
+  network-manager \
+  network-manager-applet \
+  nftables \
+  openssh-client \
+  pavucontrol \
+  pipewire \
+  pipewire-pulse \
+  pipewire-audio \
+  pipewire-alsa \
+  psmisc \
+  slurp \
+  swappy \
+  tar \
+  vim \
+  waybar \
+  wireplumber \
+  wf-recorder \
+  wget \
+  wlogout \
+  wofi \
+  xdg-desktop-portal-gtk \
+  xdg-desktop-portal-wlr \
+  zsh
+
+sudo apt install -y --no-install-recommends \
+  build-essential \
+  cmake \
+  g++ \
+  libpam0g-dev \
+  libcairo2-dev \
+  libdrm-dev \
+  libgbm-dev \
+  libgl1-mesa-dev \
+  libinput-dev \
+  libjpeg-dev \
+  libmagic-dev \
+  libpango1.0-dev \
+  libpugixml-dev \
+  libspa-0.2-bluetooth \
+  libspng-dev \
+  libsdbus-c++-dev \
+  libwayland-dev \
+  libwebp-dev \
+  libxkbcommon-dev \
+  make \
+  mpv \
+  ninja-build \
+  pkg-config \
+  wayland-protocols
+
+sleep 1.0
