2a47e6235dbd68d8d24d6c5aca973a321d34d83a
jwno-git <161667580+jwno-git@users.noreply.github.com>
2025-06-06 22:09:16 -0600

    Update 7.sh

--- 7.sh
+++ 7.sh
@@ -4,14 +4,12 @@ set -e
 
 cd /home/$USER/.icons/
 tar -xf BreezeX-RosePine-Linux.tar.xz
-tar -xf rose-pine-hyprcursor.tar.xz
 mv BreezeX-RosePine-Linux RosePine
 
 cd /home/$USER/.themes/
 tar -xf Tokyonight-Dark.tar.xz
 
 sudo cp -r /home/$USER/.icons/RosePine /usr/share/icons/
-sudo cp -r /home/$USER/.icons/rose-pine-hyprcursor /usr/share/icons/
 sudo cp -r /home/$USER/.themes/Tokyonight-Dark /usr/share/themes/
 
 sudo mkdir -p /root/.src
@@ -19,7 +17,7 @@ sudo mv /home/$USER/.root/.config /root/
 sudo mv /home/$USER/.root/.zshrc /root/
 sudo mv /home/$USER/.root/.vimrc /root/
 sudo mv /home/$USER/.root/debianlogo.png /root/
-sudo cp -r /home/$USER/.zsh /root/.src/
+sudo cp -r /home/$USER/.src/.zsh /root/.src/
 
 sudo chmod +x /home/$USER/.local/scripts/toggle_record.sh
 sudo chmod +x /home/$USER/.local/scripts/toggle_notes.sh
