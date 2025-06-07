0201841849781afa6868ee6307fa63dd09d2c0b2
jwno-git <161667580+jwno-git@users.noreply.github.com>
2025-06-06 22:01:21 -0600

    Update volume-notify.sh

--- .local/scripts/volume-notify.sh
+++ .local/scripts/volume-notify.sh
@@ -6,6 +6,6 @@ wpctl set-volume @DEFAULT_AUDIO_SINK@ "$1"
 # Get the new volume
 VOLUME=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%.0f%%", $2 * 100}')
 
-# Send a notification
-dunstify -r 91190 -u low "Volume: $VOLUME"
-
+# Send a notification using notify-send (works with mako)
+# Use app-name volume to trigger mako's special volume styling
+notify-send -a volume -h int:value:${VOLUME%\%} "Volume: $VOLUME"
